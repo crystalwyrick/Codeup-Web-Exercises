825f76741dfa1b24d2a177c23beea38e96c4d007
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2015-10-12 15:10:15 -0500

    calculator arrays objects

---
+++ public/js/split_join_arrays.js
@@ -0,0 +1,14 @@
+// todo:
+// Copy the array of planets you created in the last lesson.
+var planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];	
+
+
+// todo: Join the planets array with pipes (|) to create a variable named 'planetsAsString'.
+var planetAsString = planets.join("|");
+console.log(planetsAsString);
+
+// todo: Split the 'planetsAsString' variable by pipes (|) to create a variable named 'planetsAsArray'.
+console.log(planetsAsArray);
+
+
+
