5e77918bc5e01e879282e579fdffbe4178c64547
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2015-10-15 11:44:46 -0500

    exercises

---
+++ public/js/jquery_exercises.js
@@ -0,0 +1,11 @@
+
+// ID Selector exercise:
+// $(document).ready(function() {
+// 	var contents = $("#li");
+// 	alert(contents.html());
+// })
+
+// Class Selector Exercise:
+   $(document).ready(function() {
+            $('.codeup').css('border', 'solid red 12px');
+        });
