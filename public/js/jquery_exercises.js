cca85808b4286fd9d2666c39e8c7de77dbd3bb1c
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2015-10-16 14:46:04 -0500

    jquery exercises

--- public/js/jquery_exercises.js
+++ public/js/jquery_exercises.js
@@ -6,6 +6,81 @@
 // })
 
 // Class Selector Exercise:
-   $(document).ready(function() {
-            $('.codeup').css('border', 'solid red 12px');
-        });
+// $(document).ready(function() {
+//     $('.codeup').css('border', 'solid red 12px');
+// });
+
+// Element Selector Exercise:
+// 	$(document).ready(function() {
+// 		$("li").css("font-size", "20px");
+// 		$("h1, p, li").css("background-color", "yellow");
+// 		var contents = $("h1");
+// 		alert(contents.html());
+// });
+
+// Multiple Selector Exercise:
+// $(document).ready(function() {
+// 	$("*").css("background-color","yellow");
+// });
+
+// Mouse Events Exercise:
+	// $(document).ready(function(){
+	// 	$("h1").click(function(){
+	// 		$(this).css("background-color","#f00");
+	// 	})
+	// 	$("p").dblclick(function(){
+	// 		$(this).css("font-size","18px");
+	// 	})
+	// 	$("li").hover(function(){
+	// 		$(this).css("color","#f00")
+	// 	},
+	// 	function(){
+	// 		$(this).css("color","#000")
+	// 	})
+	// });
+
+// Konami Exercise:
+	"use strict"
+	
+	var konamiCode = [38,38,40,40,37,39,37,39,66,65,13,91]
+	var keysSelected = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
