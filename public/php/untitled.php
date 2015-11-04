eed5a0f02bb1d9baf0aaadc8eb08a34b5920359f
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2015-11-04 11:01:12 -0600

    file reorg

---
+++ public/php/untitled.php
@@ -0,0 +1,17 @@
+ <?php
+
+ $x = 0;
+ $y = 5;
+ $z = 10;
+
+
+ // if $x < $y < $z then echo "{$x} < {$y} < {$z}\n";
+if ($x < $y && $y < $z) {
+    "{$x} < {$y} < {$z}\n";
+}
+ // if $x is greater than 0 OR less than 10, echo the result as a sentence "$x is greater than 0 OR less than 10".
+
+ // repeat the if statement for $y and $z.
+if ($x > 0 || $x < 10) {
+    "$x is greater than 0 OR less than 10";
+}
