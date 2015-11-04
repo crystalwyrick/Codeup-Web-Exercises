3dcd23c1aff0da1768e8f0c82c5f479c3b509009
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2015-11-04 14:56:29 -0600

    PHP High Low Game - added arguments

--- public/php/High_Low.php
+++ public/php/High_Low.php
@@ -1,25 +1,39 @@
 <?php
 
+$min = 1;
+$max = 100;
 
+if ($argc != 3) {
+	die ("Incorrect number of arguments passed. Please enter a min and max" . PHP_EOL);
+}
+if ($argc == 3) {
+	$min = $argv[1];
+	$max = $argv[2];
 
+	if (!((is_numeric($min)) && (is_numeric($max)))) {
+		die ("Arguments representing min and mix must be numeric." . PHP_EOL);
+	}
+}
 
-// The specs for the game are:
 $tries = 0;
-$number = mt_rand (1 , 100);
-echo "\$random value is $random" . PHP_EOL;
-fwrite(STDOUT, 'Guess a number between 1 and 100 ');
+$number = mt_rand ($min , $max);
+	if ($min > $max) {
+		$number = mt_rand ($max , $min);
+	}
+
+	fwrite(STDOUT, "Guess a number between $min and $max " . PHP_EOL);
+	
 	do { 
 		$userGuess = trim(fgets(STDIN));
 		if (!is_numeric($userGuess)) {
 			fwrite(STDOUT, 'You must guess a number ');
 		} else {
-
 			fwrite(STDOUT, 'Guess again ');
 			if ($userGuess < $number ){
-				fwrite(STDOUT, "HIGHER\n");
+				fwrite(STDOUT, "but HIGHER\n");
 			}
 			if ($userGuess > $number ){
-				fwrite(STDOUT, "LOWER\n");
+				fwrite(STDOUT, "but LOWER\n");
 			}
 			if ($userGuess == $number ){
 				fwrite(STDOUT, "GOOD GUESS!\n");
@@ -34,17 +48,5 @@ echo "Number of tries: $tries " . PHP_EOL;
 
 exit(0);
 
-// game picks a random number between 1 and 100.
-// prompts user to guess the number
-// if user's guess is less than the number, it outputs "HIGHER"
-// if user's guess is more than the number, it outputs "LOWER"
-// if a user guesses the number, the game should declare "GOOD GUESS!"
-// Hints:
 
-// Using conditionals and loops here is important
-// Random numbers can be made with the internal rand() function
-// If user is right, tell them they won
-// While they are wrong, give them hints and keep asking
-// Use exit(0) to end the game
-// If you get stuck in game, ctrl-c will exit.
 
