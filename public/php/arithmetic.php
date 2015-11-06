1dae9353da757361acc9e09e24d443c9642c72db
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2015-11-06 10:17:08 -0600

    PHP validating functions arguments - arithmetic exercise

--- public/php/arithmetic.php
+++ public/php/arithmetic.php
@@ -2,37 +2,67 @@
 
 function add($a, $b)
 {
-    return $a + $b;
+	if (is_numeric($a) && is_numeric($b)) {
+    	return $a + $b;
+    } else {
+    	return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 function subtract($a, $b)
 {
+	if (is_numeric($a) && is_numeric($b)) {
     return $a - $b;
+    } else {
+    	return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 function multiply($a, $b)
 {
+	if (is_numeric($a) && is_numeric($b)) {
 	return $a * $b;
+	} else {
+    	return "ERROR: Both arguments must be numbers\n";
+    }
+}
     // Refactor to use your add() function and a for() loop
 function multiplyWithFor($a, $b)
-    
+{    
     $sum = 0;
     for ($i=0; $i<$b; $i++) { 
     	$sum = $sum + $a;
     }
+    if (is_numeric($a) && is_numeric($b)) {
     return $sum;
+    } else {
+    	return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
 function divide($a, $b)
 {
+	if (is_numeric($a) && is_numeric($b)) {
     return $a/$b;
+    } else {
+    	return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
-echo add (2,4) . PHP_EOL;
-echo subtract (3,5) . PHP_EOL;
+function modulus($a, $b)
+{
+	if (is_numeric($a) && is_numeric($b)) {
+	return $a%$b;
+	} else {
+    	return "ERROR: Both arguments must be numbers\n";
+    }
+}
+
+echo add (6,3) . PHP_EOL;
+echo subtract (6,3) . PHP_EOL;
 echo multiply (6,3) . PHP_EOL;
 echo divide (6,3) . PHP_EOL;
-echo modulus (6,3) . PHP_EOL;
+echo modulus (a,3) . PHP_EOL;
 
 /**
 *
